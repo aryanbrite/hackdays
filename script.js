c24810a029d6a029014c7d39a8ba3727be643ba4
Aryan <aryanbrite@gmail.com>
2026-06-28 15:24:19 +0530

    added live time updation

--- script.js
+++ script.js
@@ -7,17 +7,21 @@ const indiagate = document.getElementById("indiagate");
 const wave = document.getElementById("wave");
 const when = document.getElementById("when");
 const time1 = document.getElementById("time1");
-const now = new Date();
-const event = new Date("2026-07-25T09:00:00");
-const diff = event - now;
-const day = diff / (1000 * 60 * 60 * 24);
-const days = Math.floor(day);
-const hour = (day - days) * 24
-const hours = Math.floor(hour)
-const min = (hour - hours) * 60
-const mins = Math.floor(min)
-const sec = (min - mins) * 60
-const secs = Math.floor(sec)
+function countdown() {
+    const now = new Date();
+    const event = new Date("2026-07-25T09:00:00");
+    const diff = event - now;
+    const day = diff / (1000 * 60 * 60 * 24);
+    const days = Math.floor(day);
+    const hour = (day - days) * 24;
+    const hours = Math.floor(hour);
+    const min = (hour - hours) * 60;
+    const mins = Math.floor(min);
+    const sec = (min - mins) * 60;
+    const secs = Math.floor(sec);
+    time1.textContent=`${days}d ${hours}h ${mins}m ${secs}s`;
+}
+setInterval(countdown,1000);
 where.onclick = () => {
     wave.classList.remove("wavenew");
     indiagate.classList.add("add");
